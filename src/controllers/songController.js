1669849e38cd474e050b2e4144078fd792a10977
ICE0208 <doky3307@naver.com>
2023-04-02 17:14:07 +0900

    Implement song loading and sorting by view count

--- src/controllers/songController.js
+++ src/controllers/songController.js
@@ -1,27 +1,16 @@
 import Song from "../models/Song";
 import User from "../models/User";
 
-const tempThumb =
-  "https://images.squarespace-cdn.com/content/v1/55fc0004e4b069a519961e2d/1442590746571-RPGKIXWGOO671REUNMCB/image-asset.gif?format=300w";
-
-const musics = [
-  { title: "Title1", artist: "artist1", thumb: tempThumb, views: 101 },
-  { title: "Title2", artist: "artist2", thumb: tempThumb, views: 53 },
-  { title: "Title3", artist: "artist3", thumb: tempThumb, views: 74 },
-  { title: "Title3", artist: "artist3", thumb: tempThumb, views: 72 },
-  { title: "Title3", artist: "artist3", thumb: tempThumb, views: 24 },
-  { title: "Title3", artist: "artist3", thumb: tempThumb, views: 56 },
-  { title: "Title3", artist: "artist3", thumb: tempThumb, views: 34 },
-  { title: "Title3", artist: "artist3", thumb: tempThumb, views: 14 },
-  { title: "Title3", artist: "artist3", thumb: tempThumb, views: 10 },
-  { title: "Title3", artist: "artist3", thumb: tempThumb, views: 62 },
-  { title: "Title3", artist: "artist3", thumb: tempThumb, views: 1 },
-  { title: "Title3", artist: "artist3", thumb: tempThumb, views: 2 },
-  { title: "Title3", artist: "artist3", thumb: tempThumb, views: 3 },
-];
-
-musics.sort((a, b) => b.views - a.views);
+const getSongs = async (req, res) => {
+  try {
+    const musics = await Song.find({}).sort({ views: "desc" });
+    return musics;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
 
 export const home = async (req, res) => {
-  return res.render("home", { pageTitle: "Home", musics: musics });
+  return res.render("home", { pageTitle: "Home", musics: await getSongs() });
 };
--- src/middlewares.js
+++ src/middlewares.js
@@ -2,5 +2,8 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "XXXX";
   res.locals.loggedInUser = req.session.user || {};
+  res.locals.getThumbUrl = (ytID) => {
+    return `https://img.youtube.com/vi/${ytID}/mqdefault.jpg`;
+  };
   next();
 };
